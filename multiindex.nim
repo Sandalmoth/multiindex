e318254e1a03e8d3a6ced0b2bfdb46b29d1392d4
Jonathan <j@localhost.localdomain>
2020-10-15 19:37:05 +0200

    rudimentary insertion and static for loop

--- multiindex.nim
+++ multiindex.nim
@@ -1,8 +1,28 @@
 import 
   macros, typetraits
 
-macro zipFields(a, b: untyped): untyped =
-  discard
+proc macroReplaceRecursive(old, to: NimNode, ast: NimNode): NimNode =
+  result = ast.copyNimTree()
+  case result.kind:
+    of nnkNone, nnkEmpty, nnkNilLit, nnkCharLit..nnkUInt64Lit, nnkFloatLit..nnkFloat64Lit, 
+        nnkStrLit..nnkTripleStrLit, nnkCommentStmt, nnkIdent, nnkSym:
+      discard
+    else:
+      for i in 0..<result.len:
+        if result[i] == old:
+          result[i] = to
+        else:
+          result[i] = macroReplaceRecursive(old, to, result[i])
+
+macro forStatic(index: untyped, a, b: static int, body: untyped): untyped =
+  result = nnkStmtList.newTree()
+  for j in a..<b:
+    result.add(
+      macroReplaceRecursive(index, newLit(j), body)
+    )
+
+  echo "-- Unrolled:", body.repr
+  echo "-- To:", result.repr, '\n'
 
 # unclear why t.type.arity sometimes cannot be used directly.
 # This makes it possible to access the number of elements in
@@ -35,9 +55,26 @@ proc add(mi: var MultiIndex; val: mi.T) =
 
   # unbalanced insertion for now
   # TODO switch to scapegoat tree
-  var k = 0
-  for field in val.fields:
-    inc k
+  var 
+    root = mi.root
+  forStatic k, 0, mi.T.len:
+    while true:
+      echo root.headers
+      if cmp(node.value[k], root.value[k]) < 0:
+        if root.headers[k].left.isNil:
+          root.headers[k].left = node
+          node.headers[k].parent = root
+          break
+        else:
+          root = root.headers[k].left
+      else:
+        if root.headers[k].right.isNil:
+          root.headers[k].right = node
+          node.headers[k].parent = root
+          break
+        else:
+          root = root.headers[k].right
+    
 
 iterator items(mi: MultiIndex, k: static int): mi.T =
   # in order traversal of the tree for field k
@@ -50,19 +87,20 @@ iterator items(mi: MultiIndex, k: static int): mi.T =
       while not node.headers[k].left.isNil:
         node = node.headers[k].left
       
-      yield node.value
+    yield node.value
 
-      leftDone = true
-      if not node.headers[k].right.isNil:
-        leftDone = false
-        node = node.headers[k].right
-      elif not node.headers[k].parent.isNil:
-        while not node.headers[k].parent.isNil and node == node.headers[k].parent.headers[k].right:
-          node = node.headers[k].parent
-        if node.headers[k].parent.isNil:
-          break
-      else:
+    leftDone = true
+    if not node.headers[k].right.isNil:
+      leftDone = false
+      node = node.headers[k].right
+    elif not node.headers[k].parent.isNil:
+      while not node.headers[k].parent.isNil and node == node.headers[k].parent.headers[k].right:
+        node = node.headers[k].parent
+      if node.headers[k].parent.isNil:
         break
+      node = node.headers[k].parent
+    else:
+      break
 
 
 
@@ -70,6 +108,8 @@ var mi: MultiIndex[(int, string)]
 
 mi.add((3, "howdy"))
 mi.add((2, "pardner"))
+mi.add((1, "xavier"))
+mi.add((4, "another"))
 
 for x in mi.items(0):
   echo x
