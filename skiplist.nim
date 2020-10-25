0af065306577ef9d767a17b1dad2a42a1c239d02
Jonathan <j@localhost.localdomain>
2020-10-25 14:15:40 +0100

    add some extra skiplist functionality

--- skiplist.nim
+++ skiplist.nim
@@ -1,5 +1,9 @@
+# skiplist data structure implemented
+# like on opendatastructures.org
+# using an interface like std::multiset
+
 import
-  bitops, math, random
+  algorithm, bitops, math, random, sequtils
 
 
 const 
@@ -26,13 +30,7 @@ proc `$`[T](m: Multiset[T]): string =
   for i in 0..<m.level:
     var walk = m.sentinel.next[i]
     var j = 0
-    echo i
     while not walk.isNil:
-      # echo fmt"{cast[int](walk):#x}"
-      # echo j, "\t", walk.value
-      # echo walk.addr.repr
-      # echo walk.next.repr
-      # echo fmt"{cast[int](walk.next[i]):#x}"
       inc j
       result = result & ' ' & $walk.value
       walk = walk.next[i]
@@ -88,7 +86,77 @@ proc insert[T](m: var Multiset[T], x: T) =
   dealloc(stack)
 
 
+proc erase[T](m: var Multiset[T], node: var ptr Node[T]) =
+  # erasing a specific node with duplicates allowed
+  # is more complex than erasing the first with a given value
+  # first find the first element with the
+  # same value and save the path
+  let
+    stack = alloc0(sizeof(ptr Node[T])*m.level)
+  var 
+    walk = m.sentinel
+    update = cast[ptr UncheckedArray[ptr Node[T]]](stack)
+  for i in countdown(m.level - 1, 0):
+    while not walk.next[i].isNil and walk.next[i].value < node.value:
+      walk = walk.next[i]
+    update[i] = walk
+  # now keep stepping forward at level 0 until we reach our element
+  while walk.next[0] != node:
+    walk = walk.next[0]
+    for i in 1..<m.level:
+      # check that we didn't step past something changing the shortest path
+      if update[i].next[i] == walk:
+        update[i] = walk
+  update[0] = walk
+  # finally erase on all levels
+  for i in countdown(m.level - 1, 0):
+    if update[i].next[i] == node:
+      update[i].next[i] = node.next[i]
+    if update[i] == m.sentinel and node.next[i].isNil:
+      dec m.level
 
+  dealloc(stack)
+  `=destroy`(node)
+  dealloc(node.next)
+  dealloc(node)
+
+
+proc find[T](m: Multiset[T], x: T): ptr Node[T] =
+  result = m.sentinel
+  for i in countdown(m.level - 1, 0):
+    while not result.next[i].isNil and result.next[i].value < x:
+      result = result.next[i]
+  result = result.next[0]
+  if not result.isNil and result.value != x:
+    result = nil
+
+
+proc count[T](m: Multiset[T], x: T): int=
+  var it = m.find(x)
+  if it.isNil:
+    return
+  while not it.isNil and it.value == x:
+    inc result
+    next(it)
+
+
+proc lower_bound[T](m: Multiset[T], x: T): ptr Node[T] =
+  result = m.sentinel
+  for i in countdown(m.level - 1, 0):
+    while not result.next[i].isNil and result.next[i].value < x:
+      result = result.next[i]
+  if result.next[0].value == x:
+    result = result.next[0]
+
+
+proc upper_bound[T](m: Multiset[T], x: T): ptr Node[T] =
+  result = m.sentinel
+  for i in countdown(m.level - 1, 0):
+    while not result.next[i].isNil and result.next[i].value <= x:
+      result = result.next[i]
+  result = result.next[0]
+  if not result.isNil and not result.next[0].isNil and result.next[0].value == x:
+    result = result.next[0]
 
 
 var
@@ -98,8 +166,8 @@ var
 m.init()
 
 const
-  N = 500
-  X = 9
+  N = 10000
+  X = 1000
 
 randomize()
 for i in 0..<N:
@@ -107,16 +175,41 @@ for i in 0..<N:
   m.insert(x)
   v.add(x)
 
+var
+  to_erase = v[0..<(v.len div 3)].sorted()
+  remainder = v[(v.len div 3)..<v.len].sorted()
+  vst = v.sorted()
 
-var 
-  f = m.first()
-  j = 0
-echo f.type
-while not f.isNil:
-  echo j, ' ', f.value
-  next(f)
-  inc j
+block:
+  var 
+    it = m.first()
+    i = 0
+  while not it.isNil:
+    assert it.value == vst[i]
+    next(it)
+    inc i
 
+echo "passed insert test"
 
-echo "level ", m.level
-# echo m
+block:
+  for x in to_erase:
+    var it = m.find(x)
+    m.erase(it)
+
+block:
+  var 
+    it = m.first()
+    i = 0
+  while not it.isNil:
+    assert it.value == remainder[i]
+    next(it)
+    inc i
+
+echo "passed erase test"
+
+block:
+  for i in 0..<1000:
+    let x = rand(X)
+    assert m.count(x) == remainder.count(x)
+
+echo "passed count test"
